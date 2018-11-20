4834f9e194bbbdb700b81395218950ab18cf157e
asdfilippo <aline.filippo@gmail.com>
2018-11-20 15:14:24 -0600

    First basic version of the main program

---
+++ main.py
@@ -0,0 +1,12 @@
+# Forecast module
+import requests, pprint
+import day, fiveDays, interface
+from tkinter import *
+
+window = Tk()
+
+window1 = interface.firstWindow(window)
+
+window.mainloop()
+
+
