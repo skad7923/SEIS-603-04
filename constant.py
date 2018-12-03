099685d7b7a3b938d1800bd2abe545fe9cd9388f
asdfilippo <aline.filippo@gmail.com>
2018-12-03 12:22:46 -0600

    New file to store constants

---
+++ constant.py
@@ -0,0 +1,27 @@
+CELSIUS = "metric"
+FAHRENHEIT = "imperial"
+
+TODAY_WEATHER = "todayWeather"
+TOMORROW_WEATHER = "tomorrowWeather"
+FIVE_DAYS_WEATHER = "5daysForecast"
+
+API_TODAY_WEATHER = 'http://api.openweathermap.org/data/2.5/weather?zip={}&appid=20376eb779c0af83f41165d793e494c0&units={}'
+API_FIVE_DAYS_WEATHER = 'http://api.openweathermap.org/data/2.5/forecast?zip={}&appid=20376eb779c0af83f41165d793e494c0&units={}'
+
+TEMP_LOW_F_1 = 25
+TEMP_LOW_F_2 = 40
+TEMP_LOW_F_3 = 60
+TEMP_LOW_F_4 = 80
+
+TEMP_LOW_C_1 = -4
+TEMP_LOW_C_2 = 4
+TEMP_LOW_C_3 = 15.6
+TEMP_LOW_C_4 = 26.7
+
+MEME_SNOW = "memes/weather_snow.jpg"
+MEME_RAIN = "memes/weather_rain.jpg"
+MEME_TEMP1 = "memes/weather_temp1.png"
+MEME_TEMP2 = "memes/weather_temp2.jpg"
+MEME_TEMP3 = "memes/weather_temp3.jpg"
+MEME_TEMP4 = "memes/weather_temp4.jpg"
+MEME_TEMP5 = "memes/weather_temp5.jpg"
